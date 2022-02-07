bd7e6bda216fe3d684c85fb5ee336c23c869814c
e-sushi <officialaxos7@gmail.com>
2022-02-06 20:17:48 -0500

    add new tuple

--- src/utils/pair.h
+++ src/utils/pair.h
@@ -70,4 +70,68 @@ static pair<T, U> make_pair(const T& first, const U& second) {
 	return pair<T, U>(first, second);
 }
 
+//TODO make an index sequence thing of our own
+#include <utility>
+
+//from https://ldionne.com/2015/11/29/efficient-parameter-pack-indexing/
+template <std::size_t n, typename = std::make_index_sequence<n>>
+struct nth_element_impl;
+
+template <std::size_t n, std::size_t ...ignore>
+struct nth_element_impl<n, std::index_sequence<ignore...>> {
+    template <typename Tn>
+    static Tn f(decltype((void*)ignore)..., Tn*, ...);
+};
+
+template <typename T>
+struct wrapper { using type = T; };
+
+template <std::size_t n, typename ...T>
+using nth_element = typename decltype(
+    nth_element_impl<n>::f(static_cast<wrapper<T>*>(0)...)
+)::type;
+
+template<typename... T>
+constexpr u64 Sum(){
+	return (sizeof(T) + ...);
+}
+
+template<typename T, typename... Args>
+constexpr T RunConstructor(Args... args){
+	return T(args...);
+}
+
+template<typename... T>
+struct tuple {
+	u8 raw[Sum<T...>()];
+	u64 offsets[sizeof...(T)];
+	tuple(){
+		u64 sum = 0, idx = 0;
+		((offsets[idx++] = sum, 
+		memcpy(((T*)&raw[sum]), 
+		&RunConstructor<T>(), 
+		sizeof(T)), sum+=sizeof(T)), ...);
+	}
+
+	tuple(T...args){
+		u64 sum = 0, idx = 0;
+		((offsets[idx++] = sum, memcpy(((T*)&raw[sum]), &args, sizeof(T)), sum+=sizeof(T)), ...);
+	}
+
+	template<upt n>
+	decltype(auto) get(){
+		return *(nth_element<n, T...>*)&raw[offsets[n]];
+	} 
+
+	template<upt n>
+	decltype(auto) getptr(){
+		return (nth_element<n, T...>*)&raw[offsets[n]];
+	} 
+	
+	
+
+
+};
+
+
 #endif //DESHI_PAIR_H
