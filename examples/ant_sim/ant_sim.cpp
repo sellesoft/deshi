5df20a57f29172587db21f0425fe50076f880e71
sushi <officialaxos7@gmail.com>
2022-12-31 15:12:01 -0500

    [ant_sim] progress

--- examples/ant_sim/ant_sim.cpp
+++ examples/ant_sim/ant_sim.cpp
@@ -426,7 +426,7 @@ struct{
 	vec2i visual_size;
 	vec2i visual_position;
 }rendering;
-#define GetPixel(x,y) rendering.screen[x+y*WORLD_WIDTH]
+#define GetPixel(x,y) rendering.screen[x+(WORLD_HEIGHT-y*WORLD_WIDTH)]
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //@main
@@ -440,7 +440,6 @@ int main(int args_count, char** args){
 	g_ui->base.style.font_height = 11;
 	g_ui->base.style.text_color  = Color_White;
 
-	
 
 	//init ant_sim storage
 	action_def_arena = memory_create_arena(Megabytes(1));
@@ -480,8 +479,8 @@ int main(int args_count, char** args){
 				worldtex->style.background_color = {255,255,255,255};
 				worldtex->style.sizing = size_percent_x | size_square | size_auto_y;
 				worldtex->style.size = {100,0};
-				worldtex->style.positioning = pos_draggable_relative;
-				
+				worldtex->style.positioning = pos_draggable_fixed;
+				worldtex->style.border_width = 1;
 			}uiItemE();
 		}uiItemE();
 		uiItem* infowin = uiItemB();{
@@ -492,12 +491,9 @@ int main(int args_count, char** args){
 		}uiItemE();
 	}uiItemE();
 
-	// initialize world by spawning all items in mid air so they fall down and it looks cool
 
-	// forI(WORLD_WIDTH*WORLD_HEIGHT/2){
-	// 	vec2i pos = {rand() % WORLD_WIDTH, rand() % WORLD_HEIGHT};
-	// 	make_nonagent_entity(pos, 0, Entity_Dirt);
-	// }
+
+	// initialize world by spawning all items in mid air so they fall down and it looks cool
 
 	while(platform_update()){
 		//simulate
@@ -529,10 +525,6 @@ int main(int args_count, char** args){
 						u32 r = rand() % 3; 
 						if(r == 1) nupos.x += 1;
 						else if(r == 2) nupos.x -= 1;
-						
-						while(!move_entity(it, nupos)){
-							nupos.x += (rand()%2 ? 1 : -1);
-						}
 						GetPixel(it->pos.x,it->pos.y) = PackColorU32(255,0,255,0);
 					}break;
 					case Entity_Dirt:{
@@ -584,6 +576,7 @@ int main(int args_count, char** args){
 		// forI(WORLD_WIDTH*WORLD_HEIGHT) {
 		// 	rendering.screen[i] = PackColorU32((u32)floor(255.0*i/(WORLD_WIDTH*WORLD_HEIGHT)),50,50,255);
 		// }
+		ui_debug();
 
 
 		render_update_texture(rendering.texture, vec2i{0,0}, vec2i{WORLD_WIDTH,WORLD_HEIGHT});
