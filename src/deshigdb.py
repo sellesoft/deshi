1bafec3d0542144347cd1f9f856954e9a84dbc20
sushi <officialaxos7@gmail.com>
2023-07-03 14:49:43 -0400

    [gdb] wrap commands in try/except

--- src/deshigdb.py
+++ src/deshigdb.py
@@ -36,34 +36,37 @@ class render_bookkeeper(gdb.Command):
     def __init__(self):
         super(render_bookkeeper, self).__init__("rbookkeep", gdb.COMMAND_USER, gdb.COMPLETE_COMMAND)
     def invoke(self, arg, tty):
-        val = gdb.lookup_symbol("renderBookKeeperArray")[0]
-        if val == None:
-            print("Could not find the symbol 'renderBookKeeperArray")
-            return
-        count = gdb.lookup_symbol("renderBookKeeperCount")[0]
-        if count == None:
-            print("Could not find symbol 'renderBookKeeperCount")
-            return
-        vertex_start = int(gdb.parse_and_eval("(Vertex2*)renderTwodVertexArray"))
-        vertex_size = int(gdb.parse_and_eval("sizeof(Vertex2)"))
-        index_start = int(gdb.parse_and_eval("(u32*)renderTwodIndexArray"))
-        index_size = int(gdb.parse_and_eval("sizeof(u32)"))
-        for i in range(int(count.value())):
-            book = gdb.parse_and_eval(f"renderBookKeeperArray[{i}]")
-            if int(book["type"]) == int(gdb.parse_and_eval("RenderBookKeeper_Vertex")):
-                file = book['file']
-                line = book['line']
-                start = str((int(book['vertex']['start']) - vertex_start)/vertex_size)
-                count = float(book['vertex']['count'])
-                print(f"vertex: s{start} c{count} from {file}:{line}")
-            elif int(book["type"]) == int(gdb.parse_and_eval("RenderBookKeeper_Index")):
-                file = book['file']
-                line = book['line']
-                start = str((int(book['index']['start']) - index_start)/index_size)
-                count = float(book['index']['count'])
-                print(f" index: s{start} c{count} from {file}:{line}")
-            # elif int(book["type"]) == int(gdb.parse_and_eval("RenderBookKeeper_Cmd")):
-            #     print(f"   cmd: c{book['cmd']} from {book['file']}:{book['line']}")
+        try:
+            val = gdb.lookup_symbol("renderBookKeeperArray")[0]
+            if val == None:
+                print("Could not find the symbol 'renderBookKeeperArray")
+                return
+            count = gdb.lookup_symbol("renderBookKeeperCount")[0]
+            if count == None:
+                print("Could not find symbol 'renderBookKeeperCount")
+                return
+            vertex_start = int(gdb.parse_and_eval("(Vertex2*)renderTwodVertexArray"))
+            vertex_size = int(gdb.parse_and_eval("sizeof(Vertex2)"))
+            index_start = int(gdb.parse_and_eval("(u32*)renderTwodIndexArray"))
+            index_size = int(gdb.parse_and_eval("sizeof(u32)"))
+            for i in range(int(count.value())):
+                book = gdb.parse_and_eval(f"renderBookKeeperArray[{i}]")
+                if int(book["type"]) == int(gdb.parse_and_eval("RenderBookKeeper_Vertex")):
+                    file = book['file']
+                    line = book['line']
+                    start = str((int(book['vertex']['start']) - vertex_start)/vertex_size)
+                    count = float(book['vertex']['count'])
+                    print(f"vertex: s{start} c{count} from {file}:{line}")
+                elif int(book["type"]) == int(gdb.parse_and_eval("RenderBookKeeper_Index")):
+                    file = book['file']
+                    line = book['line']
+                    start = str((int(book['index']['start']) - index_start)/index_size)
+                    count = float(book['index']['count'])
+                    print(f" index: s{start} c{count} from {file}:{line}")
+                # elif int(book["type"]) == int(gdb.parse_and_eval("RenderBookKeeper_Cmd")):
+                #     print(f"   cmd: c{book['cmd']} from {book['file']}:{book['line']}")
+        except Exception as e:
+            print(f"error: {e}")
 render_bookkeeper()
         
 
@@ -95,14 +98,17 @@ class show_generic_heap(gdb.Command):
         super(show_generic_heap, self).__init__("show_generic_heap", gdb.COMMAND_USER, gdb.COMPLETE_COMMAND)
         
     def invoke(self, arg, tty):
-        heap = gdb.parse_and_eval("deshi__memory_generic_expose()")
-        node = heap['last_chunk']
-        out = []
-        while 1:
-            out.append(f"{node} : {node['size']}")
-            node = node['prev']
-            if not node: break
-        print("\n".join(out))
+        try:
+            heap = gdb.parse_and_eval("deshi__memory_generic_expose()")
+            node = heap['last_chunk']
+            out = []
+            while 1:
+                out.append(f"{node} : {node['size']}")
+                node = node['prev']
+                if not node: break
+            print("\n".join(out))
+        except Exception as e:
+            print(f"error: {e}")
 show_generic_heap()
 
 class get_closest_generic_header(gdb.Command):
@@ -110,25 +116,28 @@ class get_closest_generic_header(gdb.Command):
         super(get_closest_generic_header, self).__init__("get_closest_generic_header", gdb.COMMAND_USER, gdb.COMPLETE_EXPRESSION)
 
     def invoke(self, arg, tty):
-        val = gdb.parse_and_eval(arg)
-        if val == None:
-            print("invalid expression")
-            return
-        heap = gdb.parse_and_eval("deshi__memory_generic_expose()")
-        node = heap['last_chunk']
-        mindist = 999999999999999
-        minnode = None
-        print(val.type)
-        u8type = gdb.lookup_type("u8").pointer()
-        val = val.cast(u8type)
-        while 1:
-            dist = abs(val - node.cast(u8type))
-            if dist < mindist:
-                mindist = dist
-                minnode = node
-            node = node['prev']
-            if not node: break
-        # gdb.add_history(minnode)
-        gdb.execute(f"p (MemChunk*){minnode}")
+        try:
+            val = gdb.parse_and_eval(arg)
+            if val == None:
+                print("invalid expression")
+                return
+            heap = gdb.parse_and_eval("deshi__memory_generic_expose()")
+            node = heap['last_chunk']
+            mindist = 999999999999999
+            minnode = None
+            print(val.type)
+            u8type = gdb.lookup_type("u8").pointer()
+            val = val.cast(u8type)
+            while 1:
+                dist = abs(val - node.cast(u8type))
+                if dist < mindist:
+                    mindist = dist
+                    minnode = node
+                node = node['prev']
+                if not node: break
+            # gdb.add_history(minnode)
+            gdb.execute(f"p (MemChunk*){minnode}")
+        except Exception as e:
+            print(f"error: {e}")
 get_closest_generic_header()
         
