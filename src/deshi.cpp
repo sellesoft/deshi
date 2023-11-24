2ddb936b9fb84f576cd21c77fa5d785ee83eaae2
sushi <officialaxos7@gmail.com>
2023-11-23 19:10:48 -0800

    [misc] merge

--- src/deshi.cpp
+++ src/deshi.cpp
@@ -329,6 +329,7 @@ local DeshiStage deshiStage = DS_NONE;
 #include "core/scene.cpp"
 #include "core/ui.cpp"
 #include "core/render.cpp"
+#include "core/graphics.cpp"
 #include "core/commands.cpp" //NOTE(delle) this should be the last include so it can reference .cpp vars
 
 
@@ -441,17 +442,6 @@ local DeshiStage deshiStage = DS_NONE;
 #else //#elif DESHI_DIRECTX12 //#elif DESHI_OPENGL //#if DESHI_VULKAN
 #  error "no renderer selected"
 #endif //#else //#elif DESHI_DIRECTX12 //#elif DESHI_OPENGL //#if DESHI_VULKAN
-
-//// core cpp ////
-#include "core/memory.cpp"
-#include "core/console.cpp"
-#include "core/assets.cpp"
-#include "core/scene.cpp"
-#include "core/ui.cpp"
-#include "core/render.cpp"
-#include "core/graphics.cpp"
-#include "core/commands.cpp" //NOTE(delle) this should be the last include so it can reference .cpp vars
-
 
 //// global definitions ////
 local Time deshi_time;
