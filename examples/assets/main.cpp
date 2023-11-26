60aff0127448600439e5d8d6af624ddff6836d6e
sushi <officialaxos7@gmail.com>
2023-11-25 16:57:53 -0800

    [examples] update assets example to use reworked assets api

--- examples/assets/main.cpp
+++ examples/assets/main.cpp
@@ -21,15 +21,17 @@
 #include "core/input.h"
 #include "core/logger.h"
 #include "core/platform.h"
-#include "core/render.h"
+#include "core/graphics.h"
 #include "core/assets.h"
 #include "core/threading.h"
-#include "core/camera.h"
 #include "core/time.h"
 #include "core/ui.h"
 #include "core/ui_widgets.h"
 #include "core/window.h"
 #include "math/math.h"
+
+// TODO(sushi) remove when assets no longer uses this internally
+#include "external/stb/stb_ds.h"
 
 struct {
 	f32 time;
@@ -42,41 +44,45 @@ int main() {
 	logger_init();
 	Window* win = window_create(str8l("assets example"));
 	window_show(win);
-	render_init_x(win);
-	assets_init_x(win);
+	graphics_init(win);
+	assets_init(win);
+	cmd_init();
+
+	Shader* fancy_null_fragment = assets_shader_load_from_file(str8l("null_fancy.frag"), ShaderType_Fragment);
 
 	ShaderStages stages = {0};
-	stages.vertex.filename = str8l("null.vert");
-	stages.fragment.filename = str8l("null_fancy.frag");
-	array_init(stages.fragment.resources, 2, deshi_allocator);
-	array_count(stages.fragment.resources) = 2;
-	stages.fragment.resources[0] = ShaderResourceType_Texture;
-	stages.fragment.resources[1] = ShaderResourceType_UBO;
+	stages.vertex =	assets_shader_null_vertex();
+	stages.fragment = fancy_null_fragment;
+	array_init_with_elements<ShaderResourceType>(stages.fragment->resources, {
+			ShaderResourceType_Texture,
+			ShaderResourceType_UBO}, deshi_allocator);
 
 	UBO* ubo_resource0 = assets_ubo_create(sizeof(ubo));
 	UBO* ubo_resource1 = assets_ubo_create(sizeof(ubo));
 
 	Texture* alex = assets_texture_create_from_file_simple(str8l("alex.png"));
 
-	ShaderResource* resources;
-	array_init(resources, 2, deshi_temp_allocator);
-	array_count(resources) = 2;
+	auto resources = array<ShaderResource>::create_with_count(2, deshi_temp_allocator);
 	resources[0].type = ShaderResourceType_Texture;
 	resources[0].texture = alex;
 	resources[1].type = ShaderResourceType_UBO;
 	resources[1].ubo = ubo_resource0;
 
-	Material* fancy_null0 = assets_material_create_x(str8l("fancy null"), stages, resources);
+	Material* fancy_null0 = assets_material_create(str8l("fancy null"), stages, resources.ptr);
 
 	resources[1].ubo = ubo_resource1;
-	Material* fancy_null1 = assets_material_duplicate(str8l("fancy null 2"), fancy_null0, resources);
+	Material* fancy_null1 = assets_material_duplicate(str8l("fancy null 2"), fancy_null0, resources.ptr);
 
 	Model* box = assets_model_create_from_obj(str8l("data/models/box.obj"), 0);
 
 	auto view = Math::LookAtMatrix({0,0,0}, {0,0,1}).Inverse();
 	assets_update_camera_view(&view);
-	auto proj = Camera::MakePerspectiveProjectionMatrix(win->width, win->height, 90, 1000, 0.1);
+	auto proj = Math::PerspectiveProjectionMatrix(win->width, win->height, 90, 0.1, 1000);
 	assets_update_camera_projection(&proj);
+
+	cmd_run(str8l("mat_list"));
+	cmd_run(str8l("shader_list"));
+	cmd_run(str8l("texture_list"));
 
 	while(platform_update()) {	
 		mat4 transform0 = mat4::TransformationMatrix(
@@ -95,12 +101,44 @@ int main() {
 		ubo.mix = Vec4(0, 1, 0, 1);
 		assets_ubo_update(ubo_resource1, &ubo);
 
-		box->batch_array[0].material = fancy_null0;
-		assets_model_render(win, box, &transform0);
-		box->batch_array[0].material = fancy_null1;
-		assets_model_render(win, box, &transform1);
 		
-		render_update_x(win);
+		{ using namespace graphics::cmd;
+			begin_render_pass(win, graphics_render_pass_of_window_presentation_frames(win), graphics_current_present_frame_of_window(win));
+		
+			box->batch_array[0].material = fancy_null0;
+
+			forI(array_count(box->batch_array)) {
+				auto b = box->batch_array[i];
+				bind_pipeline(win, b.material->pipeline);
+				set_viewport(win, vec2::ZERO, Vec2(win->width, win->height));
+				set_scissor(win, vec2::ZERO, Vec2(win->width, win->height));
+				bind_descriptor_set(win, 0, g_assets->view_proj_ubo);
+				push_constant(win, &transform0, {GraphicsShaderStage_Vertex, sizeof(mat4), 0});
+				bind_vertex_buffer(win, box->mesh->vertex_buffer);
+				bind_index_buffer(win, box->mesh->index_buffer);
+				bind_descriptor_set(win, 1, b.material->descriptor_set);
+				draw_indexed(win, b.index_count, b.index_offset, 0);
+			}
+
+			box->batch_array[0].material = fancy_null1;
+
+			forI(array_count(box->batch_array)) {
+				auto b = box->batch_array[i];
+				bind_pipeline(win, b.material->pipeline);
+				set_viewport(win, vec2::ZERO, Vec2(win->width, win->height));
+				set_scissor(win, vec2::ZERO, Vec2(win->width, win->height));
+				bind_descriptor_set(win, 0, g_assets->view_proj_ubo);
+				push_constant(win, &transform1, {GraphicsShaderStage_Vertex, sizeof(mat4), 0});
+				bind_vertex_buffer(win, box->mesh->vertex_buffer);
+				bind_index_buffer(win, box->mesh->index_buffer);
+				bind_descriptor_set(win, 1, b.material->descriptor_set);
+				draw_indexed(win, b.index_count, b.index_offset, 0);
+			}
+
+			end_render_pass(win);
+		}
+		
+		graphics_update(win);
 	}
 }
 
