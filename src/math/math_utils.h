48eb35fbaa5772d830dac47fb7abe7e5d494893c
dellevelled <delle.caj@gmail.com>
2023-11-26 18:43:11 -0800

    [math] Disable SSE in deshi since it's broken for matrix multiplication

--- src/math/math_utils.h
+++ src/math/math_utils.h
@@ -14,7 +14,7 @@
 /* MSVC supports SSE in amd64 mode or _M_IX86_FP >= 1 (2 means SSE2) */
 #    if defined(_M_AMD64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
 //#pragma message("_MSC_VER && (defined(_M_AMD64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1))")
-#      define DESHI_USE_SSE 1 
+#      define DESHI_USE_SSE 0
 #      include <xmmintrin.h>
 #      include <pmmintrin.h>
 #      include <smmintrin.h>
@@ -22,7 +22,7 @@
 #  else//_MSC_VER
 #    ifdef __SSE__ /* non-MSVC usually #define __SSE__ if it's supported */
 //#pragma message("!_MSC_VER && __SSE__")
-#      define DESHI_USE_SSE 1
+#      define DESHI_USE_SSE 0
 #      include <xmmintrin.h>
 #      include <pmmintrin.h>
 #      include <smmintrin.h>
