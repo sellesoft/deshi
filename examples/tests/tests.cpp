1a69ecd1553e6216f7dfa5417b3108dddafb77b5
dellevelled <dellevelled@yahoo.com>
2024-07-22 21:35:46 -0700

    Move the new math module to the core folder

--- examples/tests/tests.cpp
+++ examples/tests/tests.cpp
@@ -3,7 +3,7 @@
 //#define DESHI_MATH_DISABLE_LIBC
 //#define DESHI_MATH_DISABLE_SSE
 //#define DESHI_MATH_DISABLE_CPP
-#include "math/math2.h"
+#include "core/math2.h"
 
 int main(int args_count, char** args){
 	TEST_deshi_math();
